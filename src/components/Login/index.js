aaa9bdb5dddbabce16285e98ce4733d2d72d6ff8
Zishan khan <122339587+zishanverse@users.noreply.github.com>
2023-11-05 16:59:29 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,25 +3,18 @@ import {Redirect} from 'react-router-dom'
 import Cookies from 'js-cookie'
 
 class Login extends Component {
-  state = {jwt: ''}
 
-  componentDidMount() {
-    this.geToken()
-  }
-
-  geToken = async () => {
+  logIn = async() => {
     const user = {username: 'rahul', password: 'rahul@2021'}
     const option = {method: 'POST', body: JSON.stringify(user)}
     const response = await fetch('https://apis.ccbp.in/login', option)
     const data = await response.json()
-    this.setState({jwt: data.jwt_token})
-  }
-
-  logIn = () => {
-    const {jwt} = this.state
     const {history} = this.props
-    Cookies.set('jwt_token', jwt, {expires: 30})
-    history.replace('/')
+    if (response.ok) {
+      Cookies.set('jwt_token', data.jwt_token, {expires: 30})
+    history.replace('/')  
+    }
+    
   }
 
   render() {
